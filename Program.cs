a8b462de763317a7cf1e3bcad3fecc28df8365e4
Victor H Santos <135677578+VictorH-D-S@users.noreply.github.com>
2023-06-14 16:42:33 -0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,23 +8,44 @@ namespace Aula08_ListasRepeticao
 {
     class Program
     {
+        static List<string> nomes = new List<string>();
+        static int posicao;
+        static int quant = nomes.Count;
+        
         static void Main(string[] args)
         {
-
-            List<string> nomes = new List<string>();
-
-            nomes.Add("Corando Salamanca");
-            nomes.Add("Conrado Fring");
-            nomes.Add("Conrado White");
-            nomes.RemoveAt(0);
-            //nomes.RemoveAt(X) -> Remove itens de uma lista na posição especificada
-            Console.WriteLine($"Primeiro nome: {nomes[0]}");
-            Console.WriteLine($"Segundo nome: {nomes[1]}");
-            //Console.WriteLine($"Terceito nome: {nomes[2]}");
-
-            int quant = nomes.Count;
-            Console.WriteLine($"Os nomes são dog = {quant}");
             
+            Console.WriteLine("Bem vindo ao sistema de usuários.\n======================");
+            Console.WriteLine("Escolha o que deseja fazer:\n1- Cadastrar novo usuário\n2- Buscar usuário existente.");
+            string opcao = Console.ReadLine();
+            if (opcao == "1")
+            {
+                Console.WriteLine("Digite o novo cadastro:  ");
+                nomes.Add(Console.ReadLine());
+                Console.WriteLine("==========Nome Cadastrado com Sucesso!==========");
+                Console.ReadKey();
+                Console.Clear();
+                Main(args);
+                return;
+
+            }
+            if(opcao == "2")
+            {
+
+                Console.WriteLine("Insira a posição em que deseja buscar: ");
+                posicao = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Nome: {nomes[posicao]}");
+
+                if ()
+                {
+
+                }
+
+            }
+
+            
+
+
             Console.ReadKey();
         }
         void ARREIZ()
@@ -42,6 +63,26 @@ namespace Aula08_ListasRepeticao
             Console.WriteLine($"{nomez[0]}, {nomez[1]}, {nomez[2]}");
 
             Console.ReadKey();
+        }
+
+        void LISTAS()
+        {
+            List<string> nomes = new List<string>();
+
+            nomes.Add("Corando Salamanca");
+            nomes.Add("Conrado Fring");
+            nomes.Add("Conrado White");
+            nomes.RemoveAt(0);
+            //nomes.RemoveAt(X) -> Remove e reorganiza itens de uma lista na posição especificada
+            Console.WriteLine($"Primeiro nome: {nomes[0]}");
+            Console.WriteLine($"Segundo nome: {nomes[1]}");
+            //Console.WriteLine($"Terceito nome: {nomes[2]}");
+
+            int quant = nomes.Count;
+            Console.WriteLine($"Os nomes são dog = {quant}");
+            
+            Console.ReadKey();
+
         }
     }
 }
